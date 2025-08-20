0272f97d517cea8ceff47f1002f7a9ca663e4972
Sutee Dee <sutee.dee@alumni.olin.edu>
2025-08-20 16:49:19 -0500

    Returns ingredients for one recipe using two chains.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 import streamlit as st
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai.chat_models import ChatOpenAI
@@ -12,11 +13,17 @@ llm = ChatOpenAI()
 dish = st.text_input(label="dish")
 submit = st.button("Find recipes")
 
-prompt = PromptTemplate.from_template("Find five recipes for {dish}. Only output links. No titles.")
+find_recipes_prompt = PromptTemplate.from_template("Find one recipes for {dish}. Only output links. No titles.")
+
+get_ingredients_prompt = PromptTemplate.from_template("Visit this web site {recipe} and list ingredient strings without quantity, units, or descriptors.")
 
 output_parser = StrOutputParser()
 
-chain = prompt | llm | output_parser
+find_recipes_chain = find_recipes_prompt | llm | output_parser
+
+get_ingredients_chain = get_ingredients_prompt | llm | output_parser 
+
+full_chain = RunnablePassthrough.assign(recipe=find_recipes_chain).assign(ingredients=get_ingredients_chain)
 if submit and dish:
-    result = chain.invoke({"dish": dish})
-    st.write(result)
+    result = full_chain.invoke({"dish": dish})
+    st.write(result['ingredients'])
