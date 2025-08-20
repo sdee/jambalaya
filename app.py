ef40179fda547b7ab885651464a8cb962da5f24e
Sutee Dee <sutee.dee@alumni.olin.edu>
2025-08-20 14:49:39 -0500

    Adds basic prompt.

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from langchain_core.prompts import PromptTemplate
 import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai.chat_models import ChatOpenAI
 st.title("Jambalaya")
+dish = st.text_input(label="dish")
+
+openai_key = st.secrets["OPENAI_API_KEY"]
+
+llm = ChatOpenAI()
+
+prompt = PromptTemplate.from_template("Find five recipes for {dish}. Only output links. No titles.")
+
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+result = chain.invoke({"dish": dish})
+
+st.write(result)
