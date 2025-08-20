9e8445b4096ef7ad5672d3995035244c750c9e2b
Sutee Dee <sutee.dee@alumni.olin.edu>
2025-08-20 16:34:27 -0500

    Only return results after button click

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from langchain_core.prompts import PromptTemplate
 import streamlit as st
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai.chat_models import ChatOpenAI
-st.title("Jambalaya")
-dish = st.text_input(label="dish")
 
 openai_key = st.secrets["OPENAI_API_KEY"]
 
+st.title("Jambalaya")
+
 llm = ChatOpenAI()
+
+dish = st.text_input(label="dish")
+submit = st.button("Find recipes")
 
 prompt = PromptTemplate.from_template("Find five recipes for {dish}. Only output links. No titles.")
 
 output_parser = StrOutputParser()
 
 chain = prompt | llm | output_parser
-
-result = chain.invoke({"dish": dish})
-
-st.write(result)
+if submit and dish:
+    result = chain.invoke({"dish": dish})
+    st.write(result)
